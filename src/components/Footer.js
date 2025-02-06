be28043e5a5311cfc8c204409c388cd7a6204f86
Matt Saemrow <mattsaemrow@gmail.com>
2025-02-06 14:50:30 -0600

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+export function Footer() {
+  return (
+    <div>
+      <p>Contact Info</p>
+      <p>Email: mattsaemrow@gmail.com</p>
+      <p>Phone: (507) 491-6709</p>
+    </div>
+  );
+}
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import styles from "./Header.module.css";
+
+export function Header() {
+  return (
+    <div className={styles.container}>
+      <h1>Matt Saemrow</h1>
+    </div>
+  );
+}
